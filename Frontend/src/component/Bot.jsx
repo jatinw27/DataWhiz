215763f305aefdfa80f123a676e1443a34353201
jatinw27 <jatinpunjabi27122004@gmail.com>
2025-12-03 23:30:51 +0530

    Update backend URL

--- Frontend/src/component/Bot.jsx
+++ Frontend/src/component/Bot.jsx
@@ -26,7 +26,8 @@ function Bot() {
     setLoading(true);
 
     try {
-        const res = await axios.post("http://localhost:3000/api/chatbot/message", {
+        const res = await axios.post("https://datawhiz-production.up.railway.app/api/chatbot/message", {
+
             text: userText,
             sessionId: sessionId
         });
